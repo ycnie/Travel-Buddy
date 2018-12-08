3e06616a499f2b3bdd4c8cc25e465367b2645e70
Jake Lipson <jake_lipson@yahoo.com>
2018-12-07 22:07:53 -0500

    Added methods to show UI information

--- app/src/main/java/com/testapp/travel/ui/trips/DisplayWeatherActivity.java
+++ app/src/main/java/com/testapp/travel/ui/trips/DisplayWeatherActivity.java
@@ -40,20 +40,23 @@ public class DisplayWeatherActivity extends AppCompatActivity {
     private Button btnMoreInfo;
 
     // Info
-    Trip trip;
-    String beginDateStr;
-    String endDateStr;
-    String destinationStr;
-    Double latitudeStr;
-    Double longitudeStr;
+    private Trip trip;
+    private String beginDateStr;
+    private long beginDateUnix;
+    private String endDateStr;
+    private long endDateUnix;
+    private String destinationStr;
+    private Double latitudeStr;
+    private Double longitudeStr;
+    private int numOfDays;
+    private double tripLowTemp;
+    private double tripHighTemp;
+    private boolean rain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_weather);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        //getSupportActionBar().setTitle("Weather");
 
         // UI
         tvCityName = (TextView) findViewById(R.id.tvCityName);
@@ -70,22 +73,27 @@ public class DisplayWeatherActivity extends AppCompatActivity {
         trip = (Trip) Parcels.unwrap(getIntent()
                 .getParcelableExtra("Trip"));
 
-        // Get information from trip
+        // Get information from trip object
         beginDateStr = trip.getBeginDate();
         endDateStr = trip.getEndDate();
         destinationStr = trip.getSearchDestination().getName();
         latitudeStr = trip.getSearchDestination().getLatitude();
         longitudeStr = trip.getSearchDestination().getLongitude();
 
+        // Get information about the trip
+        datesToUnixTimestamp();
+        getNumDays();
+
         // Setting UI elements
         tvCityName.setText(destinationStr);
+        tvTripDays.setText("Your trip is " + Integer.toString(numOfDays) + " days");
 
         // More information button
         btnMoreInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // [0] = month; [1] = day; [2] = year
-                String dateSplit[] = beginDateStr.split("-");
+                String dateSplit[] = dateSplitter(beginDateStr);
                 String dsDate = dateSplit[2] + "-" + dateSplit[0] + "-" + dateSplit[1];
                 String url = "https://darksky.net/details/" + Double.toString(latitudeStr) + "," + Double.toString(longitudeStr) + "/" + dsDate + "/us12/en";
                 CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
@@ -95,5 +103,46 @@ public class DisplayWeatherActivity extends AppCompatActivity {
             }
         });
 
+    }
+    /*
+     * dateSplitter(String date)
+     * @params:     date: the date in string type in the format MM-DD-YYYY
+     * @returns:    Array of strings in the format: [0]=MM; [1]=DD; [2]=YYYY
+     */
+    private String[] dateSplitter(String date){
+        return date.split("-");
+    }
+
+    /*
+     * datesToUnixTimestamp()
+     * Converts beginDateStr and endDateStr Stings to unix timestamps of type long
+     */
+    private void datesToUnixTimestamp() {
+        String splitStartDate[] = dateSplitter(beginDateStr);
+        String splitEndDate[] = dateSplitter(endDateStr);
+
+        // Converting beginDateStr to beginDateUinx
+        int newStartMonth = Integer.parseInt(splitStartDate[0]) - 1970;
+        int newStartDay = Integer.parseInt(splitStartDate[1]) - 1;
+        int newStartYear = Integer.parseInt(splitStartDate[2]) - 1;
+        beginDateUnix = (newStartYear * 31556926) + (newStartMonth * 2629743) + (newStartDay * 86400);
+
+        // Converting endDateStr to endDateUnix
+        int newEndMonth = Integer.parseInt(splitEndDate[0]) - 1970;
+        int newEndDay = Integer.parseInt(splitEndDate[1]) - 1;
+        int newEndYear = Integer.parseInt(splitEndDate[2]) - 1;
+        endDateUnix = (newEndYear * 31556926) + (newEndMonth * 2629743) + (newEndDay * 86400);
+    }
+
+    /*
+     * getNumDays()
+     * Uses beginDate and endDate to calculate the total number of days in the trip and saves the
+     * value to the global int numOfDays
+     */
+    private void getNumDays() {
+        numOfDays = 0;
+        for (long currentUnix = beginDateUnix; currentUnix <= endDateUnix; currentUnix += 86400) {
+            numOfDays++;
+        }
     }
 }
